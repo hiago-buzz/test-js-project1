5ecb166abc91c727f55691addda4cdcd116a2faa
Hiago Santos <foto.hiago@gmail.com>
2022-10-28 23:01:53 -0300

    feat: create parse query string to object

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,4 +1,23 @@
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check your params');
+  }
+
+  return `${key}=${value}`;
+};
+
 module.exports.queryString = obj =>
-  Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
+  Object.entries(obj).map(keyValueToString).join('&');
+
+module.exports.parse = stringValue =>
+  Object.fromEntries(
+    stringValue.split('&').map(item => {
+      let [key, value] = item.split('=');
+
+      if (value.indexOf(',') > -1) {
+        value = value.split(',');
+      }
+
+      return [key, value];
+    }),
+  );
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is receive', () => {
@@ -8,5 +8,53 @@ describe('Object to query string', () => {
     };
 
     expect(queryString(obj)).toBe('name=Hiago&profession=developer');
+  });
+
+  it('should create a valid query string when an array is receive', () => {
+    const obj = {
+      name: 'Hiago',
+      profession: 'developer',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe(
+      'name=Hiago&profession=developer&abilities=JS,TDD',
+    );
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Hiago',
+      profession: 'developer',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
+});
+
+describe('Query string to Object', () => {
+  it('should create a valid object when a query string is passed', () => {
+    const qs = 'name=Hiago&profession=developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'Hiago',
+      profession: 'developer',
+    });
+  });
+
+  it('should create a valid object when  exist an array value', () => {
+    const qs = 'name=Hiago&profession=developer&abilities=JS,TDD';
+
+    expect(parse(qs)).toEqual({
+      name: 'Hiago',
+      profession: 'developer',
+      abilities: ['JS', 'TDD'],
+    });
   });
 });
